455a36378de6f82c75fb7e8b8a819ecf25beadcd
ct-temp <75627973+ct-temp@users.noreply.github.com>
2021-02-03 10:23:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+apt-get -y install python3-distutils python3-dev i2c-tools libi2c-dev libjpeg-dev libjpeg-tools libfreetype-dev python3-pip python3-smbus rrdtool lighttpd chrony
 
 cp DATA_v2/* /usr/local/bin/
