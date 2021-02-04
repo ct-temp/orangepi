d7ce4a7188033e265f4192651986ac792f9b53a1
Martin <martin.stefl@ceskatelevize.cz>
2021-02-04 19:05:14 +0100

    Install

--- install.sh
+++ install.sh
@@ -13,32 +13,51 @@ echo "Instaluji balíčky do Pythonu"
 pip3 install virtualenv Pillow smbus2 psutil setuptools
 
 echo "Kopíruji data"
-cp DATA_v2/*.py /usr/local/bin/
-cp DATA_v2/*.sh /usr/local/bin/
-cp RRD_v2/* /usr/local/bin/
-cp sync_data.sh /usr/local/bin/
+cp -rf DATA_v2/*.py /usr/local/bin/
+cp -rf DATA_v2/*.sh /usr/local/bin/
+cp -rf RRD_v2/* /usr/local/bin/
+cp -rf sync_data.sh /usr/local/bin/
 
 echo "Nastavuji oprávnění"
 chmod 755 /usr/local/bin/*.sh
 
 echo "Vytvarim tmpfs"
-echo "tmpfs /var/www/html tmpfs defaults,size=20m 0 0" >> /etc/fstab
+FSTAB=`cat /etc/fstab | grep "tmpfs\ /var/www/html" | wc -l`
+if [ $FSTAB -eq 0 ];then
+	echo "tmpfs /var/www/html tmpfs defaults,size=20m 0 0" >> /etc/fstab
+else
+	echo "TMPFS exist";
+fi
 
-echo "nastavuji CRON"
-(crontab -l 2>/dev/null; echo "*/5 * * * * root /usr/local/bin/read_data.sh") | crontab -
-(crontab -l 2>/dev/null; echo "59 * * * * root /usr/local/bin/sync_data.sh") | crontab -
+echo "Settings CRON"
+CSYNC=`crontab -l | grep sync_data.sh| wc -l`
+CDATA=`crontab -l | grep read_data.sh| wc -l`
+if [ $CSYNC -eq 0 ]; then
+	(crontab -l 2>/dev/null; echo "59 * * * * root /usr/local/bin/sync_data.sh") | crontab -
+else
+	echo "Cron exist";
+fi
+
+if [ $CDATA -eq 0 ]; then
+	(crontab -l 2>/dev/null; echo "*/5 * * * * root /usr/local/bin/read_data.sh") | crontab -
+else
+	echo "Cron exist";
+fi
 
 
 echo "Systemd"
-cp DATA_v2/oledpy.service /etc/systemd/system/
+cp -rf DATA_v2/oledpy.service /etc/systemd/system/
 systemctl enable oledpy.service
 systemctl start oledpy.service
 
 
 echo "Pripravuj system"
-echo "modprobe w1-therm" >> /etc/modules-load.d/modules.conf
-echo "modprobe w1-gpio" >> /etc/modules-load.d/modules.conf
+echo "g_serial" > /etc/modules
+echo "w1-therm" >> /etc/modules
+echo "w1-gpio" >>  /etc/modules
 echo "param_w1_pin=PA14" >> /boot/armbianEnv.txt
 echo "param_w1_pin_int_pullup=1" >> /boot/armbianEnv.txt
-awk '{if ($1 ~ /^overlays=/) print $0, " w1-gpio"; else print $0}' /boot/armbianEnv.txt
+awk '{if ($1 ~ /^overlays=/) print $0, " w1-gpio i2c0"; else print $0}' /boot/armbianEnv.txt
+
+
 reboot
