0a8b28304d3986e383e5349947a257c284e7f38d
ct-temp <75627973+ct-temp@users.noreply.github.com>
2021-02-04 08:29:41 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@
 echo "Instaluji balíčky do systému"
 apt-get -y install python3-distutils python3-dev i2c-tools libi2c-dev libjpeg-dev libjpeg-tools libfreetype-dev python3-pip python3-smbus rrdtool lighttpd chrony
 
-echo "Instaluji balíčky do Pythonu"
-pip3 install virtualenv Pillow smbus2 psutil setuptools
-
 echo "Instaluji GPIo pro Zero"
 cd ~
 git clone https://github.com/nvl1109/orangepi_zero_gpio.git
 cd orangepi_zero_gpio
 python3 setup.py install
+
+echo "Instaluji balíčky do Pythonu"
+pip3 install virtualenv Pillow smbus2 psutil setuptools
 
 echo "Kopíruji data"
 cp DATA_v2/*.py /usr/local/bin/
